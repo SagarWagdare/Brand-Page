25b8b0374d648ac2d82e99c14f7dec718915fcd1
Sagar <120018715+Sagar-appsdeployer@users.noreply.github.com>
2023-12-26 15:18:47 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+    <img src="/images/brand_logo.png" alt="" />
+    <ul>
+      <li href="#">Menu</li>
+      <li href="#">Location</li>
+      <li href="#">About</li>
+      <li href="#">Contact</li>
+    </ul>
+      <button>Login</button>
+  </nav>
+  )
+}
+
+export default Navigation
